55be6a6c1d3d2a4ed31e87e29f355b54b9089699
Sean Burke <sburke@wearegenuine.com>
2020-12-01 23:20:01 -0500

    Add loading spinner to HTML / remove once JS is loaded.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,9 +6,17 @@ import Main from './layout/Main'
 import { useKeyboardStatus, useAppStatus } from './utils/hooks'
 
 const App = () => {
+  let appLoader = document.getElementById('app-loader')
   let loaded = useAppStatus()
   useKeyboardStatus()
 
+  if (loaded) {
+    appLoader.style.opacity = 0
+    setTimeout(() => {
+      appLoader.remove()
+    }, 2000)
+  }
+    
   return (
     <Router basename="/">
       <Nav />
